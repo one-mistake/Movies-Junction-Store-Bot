40f30bfcc688156c0d9ead486f54853d23e7d1aa
Vikas Chaurasia <bca2124146@smsvaranasi.in>
2024-01-27 20:31:54 +0530

    Final

--- configs.py
+++ configs.py
@@ -8,18 +8,18 @@ class Config(object):
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
 	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY"))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
-	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
+	ABOUT_BOT_TEXT = f"""__
+This is Movies Junction Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
 🤖 **My Name:** [Files Store Bot](https://telegram.me/{BOT_USERNAME})
@@ -34,18 +34,20 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 👥 **Support Channel:** [Hacking Craze](https://telegram.me/HackingCraze24_7d)
 
-📢 **Updates Channel:** [TGS Bots](https://telegram.me/TGSBots)
+📢 **Updates Channel:** [TGS Bots](https://telegram.me/TGSBots)__
 """
-	ABOUT_DEV_TEXT = f"""
+	ABOUT_DEV_TEXT = f"""__
 🧑🏻‍💻 **Developer:** @HackingCraze_Papa
 
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
+Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.__
 
 """
-	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
+	HOME_TEXT = """__
+Hi, [{}](tg://user?id={})\n\nThis is **Movies Junction Store Bot**.
 
 Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+
+**For More Movies Join : @Movies_Junction_Reborn2**__
 """
